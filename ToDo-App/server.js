982e09aa9f5d36c3fe3685f0187f3511d0985736
ashikurriyal <riyal.ashikur@gmail.com>
2025-06-29 15:59:30 +0600

    setting response header and route

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,10 +1,49 @@
 const http = require('http')
 
+const data = [
+  {
+    "title": "TypeScript",
+    "body": "Learning Node",
+    "createdAt": "29/06/25, 3:20:00 PM"
+  },
+  {
+    "title": "Prisma",
+    "body": "Learning Node",
+    "createdAt": "28/06/25, 11:45:00 AM"
+  },
+  {
+    "title": "Flutter",
+    "body": "Learning Node",
+    "createdAt": "27/06/25, 6:10:00 PM"
+  },
+  {
+    "title": "Next.js",
+    "body": "Learning Node",
+    "createdAt": "26/06/25, 9:30:00 AM"
+  },
+  {
+    "title": "GraphQL",
+    "body": "Learning Node",
+    "createdAt": "25/06/25, 9:05:00 PM"
+  }
+]
+
+
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method)
-    // res.end('Welcome to ToDo App Server')
+    console.log(req.url, req.method)
     if(req.url === '/todos' && req.method === 'GET'){
-        res.end('Here is your all todo')
+
+        //One way
+        res.writeHead(201, {
+            'content-type' : 'text/html',
+        })
+        res.end(`<h1>Hello World</h1> <h2>Hello World</h2> <h3>Hello World</h3>`)
+
+        //Another way
+        // res.setHeader('content-type', 'application/json')
+        // res.setHeader('email', 'white2@gmail.com')
+        // res.statusCode = 200;
+        // res.end(JSON.stringify(data))
     } else if(req.url === '/todos/create-todo' && req.method === 'POST'){
         res.end('New todo created successfully')
     } else {
