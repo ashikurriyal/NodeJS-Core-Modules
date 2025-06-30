9184ea933bb86f12d8c363f79401b2547ad42cbf
ashikurriyal <riyal.ashikur@gmail.com>
2025-06-30 16:04:57 +0600

    created a post method for post new todo on routing

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,60 +1,53 @@
-const http = require('http')
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const data = [
-  {
-    "title": "TypeScript",
-    "body": "Learning Node",
-    "createdAt": "29/06/25, 3:20:00 PM"
-  },
-  {
-    "title": "Prisma",
-    "body": "Learning Node",
-    "createdAt": "28/06/25, 11:45:00 AM"
-  },
-  {
-    "title": "Flutter",
-    "body": "Learning Node",
-    "createdAt": "27/06/25, 6:10:00 PM"
-  },
-  {
-    "title": "Next.js",
-    "body": "Learning Node",
-    "createdAt": "26/06/25, 9:30:00 AM"
-  },
-  {
-    "title": "GraphQL",
-    "body": "Learning Node",
-    "createdAt": "25/06/25, 9:05:00 PM"
-  }
-]
-
+const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
-    if(req.url === '/todos' && req.method === 'GET'){
+  console.log(req.url, req.method);
 
-        //One way
-        res.writeHead(201, {
-            'content-type' : 'text/html',
-        })
-        res.end(`<h1>Hello World</h1> <h2>Hello World</h2> <h3>Hello World</h3>`)
+  //Get all-todos
+  if (req.url === "/todos" && req.method === "GET") {
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
 
-        //Another way
-        // res.setHeader('content-type', 'application/json')
-        // res.setHeader('email', 'white2@gmail.com')
-        // res.statusCode = 200;
-        // res.end(JSON.stringify(data))
-    } else if(req.url === '/todos/create-todo' && req.method === 'POST'){
-        res.end('New todo created successfully')
-    } else {
-        res.end('Route Not Found')
-    }
-})
+    //One way
+    res.writeHead(201, {
+      "content-type": "application/json",
+    });
+    res.end(data);
 
+    //Another way
+    // res.setHeader('content-type', 'application/json')
+    // res.setHeader('email', 'white2@gmail.com')
+    // res.statusCode = 200;
+    // res.end(JSON.stringify(data))
 
-server.listen(5100, '127.0.0.1', () => {
-    console.log('Server listening to port 5100')
-})
+    //post a todo
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      const { title, body } = JSON.parse(data);
+      const createdAt = new Date().toLocaleString();
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+      const parsedAllTodos = JSON.parse(allTodos)
+      parsedAllTodos.push({ title, body, createdAt });
+
+      fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), { encoding: "utf-8" });
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2));
+    });
+  } else {
+    res.end("Route Not Found");
+  }
+});
+
+server.listen(5100, "127.0.0.1", () => {
+  console.log("Server listening to port 5100");
+});
 
 // todos- Get - All todo
 // todos/create-todo POST Create todo
